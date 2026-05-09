eaebe2da444d3000bac7722f7ea0acd779c70dcf
강동현 <rkdanrud1342@gmail.com>
2026-05-09 21:27:46 +0900

    JVM, Kotlin 버전업

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
-    kotlin("plugin.serialization") version "2.1.20"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
     application
 }
 
