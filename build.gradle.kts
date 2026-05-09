fdfd525140a419a776d630adb7fab26ae04ad6bb
강동현 <rkdanrud1342@gmail.com>
2026-05-09 21:23:42 +0900

    lavakord 버전업

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.schlaubi.lavakord:kord:9.1.0")
+    implementation("dev.schlaubi.lavakord:kord:9.2.0")
     implementation("dev.kord:kord-core:0.15.0")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
